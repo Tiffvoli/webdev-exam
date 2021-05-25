9c3eaa9912b877e4aa244213bdc06dc998364a23
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2021-05-26 00:45:32 +0200

    styled inputs

--- views/log-in.php
+++ views/log-in.php
@@ -7,12 +7,35 @@
      </div>
      <div id="login-page" class="login flex-column">
          <h1 class="title login-title">Log in</h1>
+
+
          <form id="form" action="/login" method="POST" onsubmit="return validate()">
-             <label for="email">Email<input name="email" id="email" type="text" placeholder="Enter your email" data-validate="email"></label>
-             <label for="password">Password<input name="password" id="password" type="password" maxlength="50" data-validate="str" data-min="2" data-max="50" placeholder="Enter your password"></label>
+             <div class="omrs-input-group">
+                 <label class="omrs-input-filled" for="email">
+                     <input required name="email" id="email" type="text" data-validate="email">
+                     <span class="omrs-input-label">Email</span>
+                 </label>
+             </div>
+             <div class="omrs-input-group">
+                 <label class="omrs-input-filled" for="password">
+                     <input required name="password" id="password" type="password" maxlength="50" data-validate="str" data-min="2" data-max="50">
+                     <span class="omrs-input-label">Password</span>
+                 </label>
+             </div>
              <button type="submit">Log in</button>
-             <div class="btn">Sign up </div>
+             <div class="btn"><a class="text-blue" href="/signup">Sign up </a></div>
          </form>
+
+         <!-- <form id="form" action="/login" method="POST" onsubmit="return validate()">
+             <div class="">
+                 <label for="email">Email<input name="email" id="email" type="text" placeholder="Enter your email" data-validate="email"></label>
+             </div>
+             <div class="">
+                 <label for="password">Password<input name="password" id="password" type="password" maxlength="50" data-validate="str" data-min="2" data-max="50" placeholder="Enter your password"></label>
+             </div>
+             <button type="submit">Log in</button>
+             <div class="btn"><a class="text-blue" href="/signup">Sign up </a></div>
+         </form> -->
      </div>
  </section>
 
